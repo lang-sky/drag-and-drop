62ce2cc7c1860171b0a296d8147daa2c1f568d6b
lang-sky <skylang121@outlook.com>
2020-02-22 21:12:11 +0800

    drag-and-drop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Draggable from './components/Draggable';
+
+const App = () => {
+  return (
+    <div>
+      <Draggable>
+        <h1>Hello</h1>
+      </Draggable>
+    </div>
+  );
+};
+
+export default App;
